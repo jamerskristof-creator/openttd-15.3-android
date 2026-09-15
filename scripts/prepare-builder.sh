814a3c90e941403121d2a5d7f1402d97a713dea9
Kristof <jamerskristof@gmail.com>
2026-09-16 01:56:47 +0200

    Disable unused Google Play Games integration for SDL2

--- scripts/prepare-builder.sh
+++ scripts/prepare-builder.sh
@@ -21,8 +21,8 @@ sed -i "s/^AppVersionCode=.*/AppVersionCode=1530001/" "$app_dir/AndroidAppSettin
 sed -i 's/^AppVersionName=.*/AppVersionName="15.3-tablet.1"/' "$app_dir/AndroidAppSettings.cfg"
 sed -i "s/^LibSdlVersion=.*/LibSdlVersion=2/" "$app_dir/AndroidAppSettings.cfg"
 sed -i "s/^MultiABI=.*/MultiABI='arm64-v8a'/" "$app_dir/AndroidAppSettings.cfg"
+sed -i "s/^GooglePlayGameServicesId=.*/GooglePlayGameServicesId=n/" "$app_dir/AndroidAppSettings.cfg"
 sed -i 's/openttd-data-14\.1-0\.zip\.xz/openttd-data-15.3-0.zip.xz/' "$app_dir/AndroidAppSettings.cfg"
 
 rm -f "$builder_dir/project/jni/application/src"
 ln -s openttd "$builder_dir/project/jni/application/src"
-
